d697fa9e90a258b502d24c5ba20f6ebd284d107a
lokeshnathyadav-ctrl <lokeshnath.yadav@gmail.com>
2026-04-30 20:23:24 +0000

    HF repo_id format in hosting.py

--- sample_qa/hosting/hosting.py
+++ sample_qa/hosting/hosting.py
@@ -3,10 +3,9 @@ import os
 #os.environ['HF_TOKEN'] = userdata.get('HF_TOKEN')
 hf_api_key = os.getenv('HF_TOKEN')
 api = HfApi(token=hf_api_key)
-repo_id="Lokeshnathy/Sample-q-a",
-repo_type="space"
+
 api.upload_folder(
-    folder_path="content/drive/MyDrive/sample_q&a/deployment",
-    repo_id=repo_id,
-    repo_type=repo_type
+    folder_path = "content/drive/MyDrive/sample_qa/deployment",
+    repo_id = "Lokeshnathy/sample-q-a",
+    repo_type = "space"
 )
