fd13ea60af2b24f9b908fc7eef7d40024eae326c
lokeshnathyadav-ctrl <lokeshnath.yadav@gmail.com>
2026-04-30 21:18:04 +0000

    app.py_update#1

--- sample_qa/deployment/app.py
+++ sample_qa/deployment/app.py
@@ -10,12 +10,9 @@ def generate_response(input_text):
     groq_api_key = groq_api_key)
   st.info(llm.invoke(input_text))
 with st.form("my_form"):
-  text=st.text_area(
-      "Enter text: ",
-      "What are the three key pieces of advice for learning how to code?",
-  )
-submitted = st.form_submit_button("Submit")
-if not groq_api_key.startswith("gsk"):
-  st.warning("Please enter your GroqChat API Key!", icon = "⚠")
-if submitted and groq_api_key.startswith("gsk"):
-  generate_response(text)
+  text=st.text_area("Enter text: ","",)
+  submitted = st.form_submit_button("Submit")
+  if not groq_api_key.startswith("gsk"):
+    st.warning("Please enter your GroqChat API Key!", icon = "⚠")
+  if submitted and groq_api_key.startswith("gsk"):
+    generate_response(text)
