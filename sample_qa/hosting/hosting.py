63aeec1e129c5ccf5c12a2a20aed7a0e10b0b575
lokeshnathyadav-ctrl <lokeshnath.yadav@gmail.com>
2026-04-30 20:33:58 +0000

    repo path correction

--- sample_qa/hosting/hosting.py
+++ sample_qa/hosting/hosting.py
@@ -5,7 +5,7 @@ hf_api_key = os.getenv('HF_TOKEN')
 api = HfApi(token=hf_api_key)
 
 api.upload_folder(
-    folder_path = "content/drive/MyDrive/sample_qa/deployment",
+    folder_path = "/content/drive/MyDrive/sample_qa/deployment",
     repo_id = "Lokeshnathy/sample-q-a",
     repo_type = "space"
 )
